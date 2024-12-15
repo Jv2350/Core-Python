a804f9539277d5648e52ff04d0565c6ddbbc0f41
Jv2350 <tp8375735@gmail.com>
2024-12-15 22:28:46 +0530

    add area of triangle program

---
+++ Variables & Operators/area_of_triangle.py
@@ -0,0 +1,8 @@
+# Python program to find the area of a triangle whose sides are given
+
+height = int(input("Enter the height of the triangle: "))
+base = int(input("Enter the base of the triangle: "))
+
+area_of_triangle = 1 / 2 * (base * height)
+
+print(f"The area of triangle with base: {base} and height: {height} is {area_of_triangle}")
