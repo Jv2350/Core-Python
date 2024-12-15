0e52dba4b9ae172ab240546928bc80e8e59e9ed9
Jv2350 <tp8375735@gmail.com>
2024-12-15 22:14:42 +0530

    add data structure example

---
+++ Intro to Python/Data Structures.py
@@ -0,0 +1,5 @@
+# Data Structures: Python supports various data structures like lists, dictionaries, sets, and tuples, which are used to store and manipulate data within a program.
+#this below is list data structure
+data=[1,2,3,4,5,6]
+
+print(data)
