0785c0e841a725f575fe81bc6e8f94a5fe5001f8
Jv2350 <tp8375735@gmail.com>
2024-12-15 22:15:35 +0530

    add control flow

---
+++ Intro to Python/Control flow.py
@@ -0,0 +1,4 @@
+# Control Flow: Programs use control flow constructs like loops and conditional statements to control the execution path based on specific conditions or criteria.
+
+if 10 < 20:
+    print("First Value is Largest")
