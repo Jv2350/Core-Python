e436e5607be0df0d918d51253201be185f0209ad
Jv2350 <tp8375735@gmail.com>
2024-12-15 22:19:16 +0530

    add mul and sum program

---
+++ Variables & Operators/mul_sum_of_nums.py
@@ -0,0 +1,10 @@
+# Calculate the multiplication and sum of two numbers
+
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+
+multiplication = num1 * num2
+addition = num1 + num2
+
+print(f"Multiplication of {num1} and {num2} is {multiplication}")
+print(f"Addition of {num1} and {num2} is {addition}")
