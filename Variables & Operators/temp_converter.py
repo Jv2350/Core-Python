bcd46304fba2f00a38cb0f7f3697adce98422ac0
Jv2350 <tp8375735@gmail.com>
2024-12-15 22:24:34 +0530

    add temp converter program

---
+++ Variables & Operators/temp_converter.py
@@ -0,0 +1,8 @@
+# Program to convert temperature from Celsius to Fahrenheit
+
+celsius = float(input("Enter temperature in Celsius: "))
+
+# conversion formula
+fahrenheit = (celsius * 9 / 5) + 32
+
+print(f"{celsius}°C is equal to {fahrenheit}°F.")
