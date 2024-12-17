56f66521cad7ea3ca4396f15fd50475219815fc0
Jv2350 <tp8375735@gmail.com>
2024-12-17 22:29:39 +0530

    add km to miles converter

---
+++ 3. Programming Statement/3 km_to_miles.py
@@ -0,0 +1,9 @@
+# Convert Kilometers to Miles
+# input kilometers
+kilometers = float(input("Enter distance in kilometers: "))
+
+# convert to miles
+miles = kilometers * 0.621371
+
+# print the distance in miles
+print(f"{kilometers} kilometers is equal to {miles} miles.")
