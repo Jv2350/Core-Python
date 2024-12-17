0a1dff0153e231ca7f15d052ec8d33396ce34268
Jv2350 <tp8375735@gmail.com>
2024-12-17 22:29:28 +0530

    add swap 2 numbers

---
+++ 3. Programming Statement/2 swap_numbers.py
@@ -0,0 +1,10 @@
+# Swap two numbers
+# input two numbers
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+
+# swap the numbers
+number1, number2 = number2, number1
+
+# print the swapped values
+print(f"After swapping: First number = {number1}, Second number = {number2}")
