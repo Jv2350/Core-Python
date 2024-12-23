e17362bbed92274641fda7e3beb130c45ab2cec3
Jv2350 <tp8375735@gmail.com>
2024-12-23 15:05:59 +0530

    add lowercase function

---
+++ 5. Functions/4 convert_name_lowercase.py
@@ -0,0 +1,9 @@
+# Convert Name to Lowercase Using lower() Function:
+
+# taking input from user
+name = input("Enter your name: ")
+
+# using lower() function to convert the string to lower case
+lowercase_name = name.lower()
+
+print(f"Your name in lowercase is: {lowercase_name}")
