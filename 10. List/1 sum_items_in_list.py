e734cb3865c4856d6b93bfffd2592fa8252d1cad
Jv2350 <tp8375735@gmail.com>
2024-12-26 23:29:28 +0530

    add sum items in list

---
+++ 10. List/1 sum_items_in_list.py
@@ -0,0 +1,5 @@
+# Python program to sum all the items in a list:
+
+input_list = list(map(int, input("Enter numbers separated by spaces: ").split()))
+
+print(f"Sum of all items: {sum(input_list)}")  # sum funciton to calculate the total
