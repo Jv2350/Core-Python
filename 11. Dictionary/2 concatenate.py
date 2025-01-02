5136f72fafcc87b29be9db8e3814f68cebac12ef
Jv2350 <tp8375735@gmail.com>
2025-01-02 22:16:39 +0530

    add concatenate

---
+++ 11. Dictionary/2 concatenate.py
@@ -0,0 +1,9 @@
+dic1 = {1: 10, 2: 20}
+dic2 = {3: 30, 4: 40}
+dic3 = {5: 50, 6: 60}
+
+# concatination
+result = {**dic1, **dic2, **dic3}
+
+# print the concatenated dictionary
+print("Concatenated Dictionary:", result)
